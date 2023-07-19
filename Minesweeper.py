6a95f5bc1b15005a055045fa134ad71a09ce82ec
Cesar Augusto Goncalves <107410135+cgoncalves94@users.noreply.github.com>
2023-07-20 00:56:29 +0100

    pre-defined the methods for the game

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,5 +1,10 @@
 class Cell:
     def __init__(self):
+        # Cell class constructor. Each cell has four attributes:
+        # is_revealed: whether the cell has been revealed by the player.
+        # is_flagged: whether the player has placed a flag on the cell.
+        # is_mine: whether the cell contains a mine.
+        # adjacent_mines: the number of mines in the cells adjacent to this one.
         self.is_revealed = False
         self.is_flagged = False
         self.is_mine = False
@@ -7,6 +12,9 @@ class Cell:
         
 class Board:
     def __init__(self, height, width):
+        # Board class constructor. The board has three attributes:
+        # height and width: the dimensions of the board.
+        # cells: a 2D list of Cell objects that make up the board.
         self.height = height
         self.width = width
         self.cells = []
@@ -16,7 +24,29 @@ class Board:
                 cell = Cell()
                 row.append(cell)
             self.cells.append(row)
-
             
-board = Board(5,5)
-print(board.cells[0][0].is_revealed)
+    #class Board methods
+    
+    def place_mines(self):
+        pass
+    
+    def calculate_adjacent_mines(self):
+        pass
+    
+    def reveal_cell(self):
+        pass
+
+    def toogle_flag(self):
+        pass
+    
+    def check_winner(self):
+        pass
+
+ #game logic functions 
+
+def game_loop(board):
+    pass
+
+def display_board(board):
+    pass
+
